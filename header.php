5af20af658149761fdf3e25e9a874b08e00c220d
vicvinc <vicvinvinc@gmail.com>
2017-11-21 01:36:35 +0800

    update favicon

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+    $template_dir = get_template_directory_uri();
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -5,13 +8,16 @@
 	<meta charset="UTF-8">
 	<title><?php wp_title(''); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="#ffffff">
 	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+
+	<link rel="icon" href="<?php echo $template_dir;?>/img/common/favicon.png" type="image/x-icon" />
+	<link rel="icon" type="image/png" href="<?php echo $template_dir;?>/img/common/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo $template_dir;?>/img/common/favicon-16x16.png" sizes="16x16">
+	
 	<link rel="manifest" href="manifest.json">
-	<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel='dns-prefetch' href='http://s.w.org/' />
 	
